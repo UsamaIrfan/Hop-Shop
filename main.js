a8769b68aa1235e05f8756e6e04b1e02e9593487
Usama Irfan <58513165+UsamaIrfan@users.noreply.github.com>
2020-06-06 14:14:17 +0500

    Updated All files

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const header = document.querySelector('header');
-const navItemBorders = document.getElementsByClassName('.border__bottom');
 const hamburgerContainer = document.querySelector('.hamburger__container');
 const hamburger = document.querySelector('.hamburger');
 const navMobile = document.querySelector('.nav__mobile');
-const sub_items_mobile = document.querySelector('.sub-item');
-const dropdownsMobile = document.querySelector('.dropdowns')
+
 window.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
         header.classList.add("shrinked");
@@ -17,7 +15,3 @@ hamburgerContainer.addEventListener('click', () => {
     hamburgerContainer.classList.toggle('open');
     navMobile.classList.toggle('open');
 });
-
-dropdownsMobile.addEventListener('click', () => {
-    sub_items_mobile.classList.toggle('open')
-})
